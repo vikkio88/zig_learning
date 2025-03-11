01b62e5851e231fb0dd722931cff628eeef42efa
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-03-11 20:38:26 +0000

    one exercise

--- ziglings/exercises/024_errors4.zig
+++ ziglings/exercises/024_errors4.zig
@@ -59,7 +59,10 @@ fn fixTooSmall(n: u32) MyNumberError!u32 {
     // If we get a TooSmall error, we should return 10.
     // If we get any other error, we should return that error.
     // Otherwise, we return the u32 number.
-    return detectProblems(n) ???;
+    return detectProblems(n) catch |err| switch (err) {
+        MyNumberError.TooSmall => 10,
+        MyNumberError.TooBig => MyNumberError.TooBig,
+    };
 }
 
 fn detectProblems(n: u32) MyNumberError!u32 {
