d5404ddc646ecd77f1f220893d618f5191662e74
Vincenzo Ciaccio <vincenzo.ciaccio@yotoplay.com>
2025-03-23 08:56:40 +0000

    helpers and test

--- bank/src/models/money.zig
+++ bank/src/models/money.zig
@@ -1,23 +1,38 @@
 const std = @import("std");
+const testing = std.testing;
+
 const enums = @import("./enums.zig");
 
 const Currency = enums.Currency;
 
-pub const Money = struct {
-    value: i32,
-    currency: Currency,
-    pub fn make(amount: i32, currency: Currency) Money {
-        return .{ .value = amount, .currency = currency };
-    }
+const Money = @This();
+value: i32,
+currency: Currency,
 
-    pub fn format(
-        money: Money,
-        comptime _: []const u8,
-        _: std.fmt.FormatOptions,
-        writer: anytype,
-    ) !void {
-        try writer.print("{} {s}\n", .{ money.value, money.currency.toSymbol() });
-    }
-};
+pub fn make(amount: i32, currency: Currency) Money {
+    return .{ .value = amount, .currency = currency };
+}
 
-test "can build money" {}
+pub fn makeEuros(amount: i32) Money {
+    return .{ .value = amount, .currency = Currency.EUR };
+}
+
+pub fn makeDollars(amount: i32) Money {
+    return .{ .value = amount, .currency = Currency.USD };
+}
+
+pub fn format(
+    money: Money,
+    comptime _: []const u8,
+    _: std.fmt.FormatOptions,
+    writer: anytype,
+) !void {
+    try writer.print("{} {s}\n", .{ money.value, money.currency.toSymbol() });
+}
+
+test "can build money with helpers" {
+    const one_eur = make(100, Currency.EUR);
+    const one_eur_2 = makeEuros(100);
+    try testing.expect(one_eur.currency == one_eur_2.currency);
+    try testing.expect(one_eur.value == one_eur_2.value);
+}
